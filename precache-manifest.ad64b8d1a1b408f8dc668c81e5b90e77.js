b15013d0afb9172d79ea9601f5508a18e052a145
leejieun94 <xaq03061@gmail.com>
2020-04-15 15:55:37 +0900

    Updates

---
+++ precache-manifest.ad64b8d1a1b408f8dc668c81e5b90e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f719d1a15c7b8a179f5cd231975d1a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fc2e278f9693167df8",
+    "url": "/static/css/main.b6beb703.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fc2e278f9693167df8",
+    "url": "/static/js/main.e25fecdb.chunk.js"
+  },
+  {
+    "revision": "8b4ace731c75fe5dbe52",
+    "url": "/static/js/runtime-main.80257f31.js"
+  }
+]);
